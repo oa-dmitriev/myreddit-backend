2d90c9b0f06432e3df743eedd491c397b2b62117
Oleg <dmitrievoleg96@gmail.com>
2021-06-05 01:26:06 +0300

    postgresql added

--- cmd/myreddit/main.go
+++ cmd/myreddit/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"log"
+	"myreddit/pkg/handler"
+	"myreddit/pkg/middleware"
+	"myreddit/pkg/post"
+	"myreddit/pkg/user"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,33 +18,36 @@ func test(c *gin.Context) {
 }
 
 func main() {
-	// userRepo := user.NewUserRepo()
-	// postRepo := post.NewPostRepo(userRepo.Db)
-	// userHandler := &handler.UserHandler{
-	// 	UserRepo: userRepo,
-	// }
-	// handler := &handler.PostHandler{
-	// 	PostRepo: postRepo,
-	// }
+	userRepo, err := user.NewUserRepo()
+	if err != nil {
+		log.Fatal("ERROR: ", err)
+	}
+	postRepo := post.NewPostRepo(userRepo.Db)
+	userHandler := &handler.UserHandler{
+		UserRepo: userRepo,
+	}
+	handler := &handler.PostHandler{
+		PostRepo: postRepo,
+	}
 
 	r := gin.Default()
 	r.GET("/", test)
-	// r.POST("/api/register", userHandler.Register)
-	// r.POST("/api/login", userHandler.Login)
+	r.POST("/api/register", userHandler.Register)
+	r.POST("/api/login", userHandler.Login)
 
-	// r.GET("/api/posts", handler.GetAll)
-	// r.GET("/api/posts/:category", handler.GetAllByCategory)
-	// r.GET("/api/posts/:category/:id", handler.GetById)
+	r.GET("/api/posts", handler.GetAll)
+	r.GET("/api/posts/:category", handler.GetAllByCategory)
+	r.GET("/api/posts/:category/:id", handler.GetById)
 
-	// authorized := r.Group("/")
-	// authorized.Use(middleware.AuthRequired())
-	// log.Println("TELL ME WAT YOU NEED")
-	// {
-	// 	authorized.POST("/api/posts", handler.NewPost)
-	// 	authorized.POST("/api/posts/:id", handler.NewComment)
-	// 	authorized.DELETE("/api/posts/:postId/:commentId", handler.DeleteComment)
-	// 	authorized.DELETE("/api/posts/:postId", handler.DeletePost)
-	// 	authorized.GET("api/posts/:category/:id/like", handler.Like)
-	// }
+	authorized := r.Group("/")
+	authorized.Use(middleware.AuthRequired())
+	log.Println("TELL ME WAT YOU NEED")
+	{
+		authorized.POST("/api/posts", handler.NewPost)
+		authorized.POST("/api/posts/:id", handler.NewComment)
+		authorized.DELETE("/api/posts/:postId/:commentId", handler.DeleteComment)
+		authorized.DELETE("/api/posts/:postId", handler.DeletePost)
+		authorized.GET("api/posts/:category/:id/like", handler.Like)
+	}
 	r.Run()
 }
--- pkg/user/repo.go
+++ pkg/user/repo.go
@@ -3,18 +3,17 @@ package user
 import (
 	"database/sql"
 	"fmt"
-	"log"
 
 	_ "github.com/lib/pq"
 	"github.com/twinj/uuid"
 )
 
 const (
-	host     = "localhost"
+	host     = "ec2-54-228-9-90.eu-west-1.compute.amazonaws.com"
 	port     = 5432
-	user     = "oleg"
-	password = "12345"
-	dbname   = "user"
+	user     = "hqkdqdwfzzmknz"
+	password = "5ea140a1ab2205f26672db6020f5297b1f696c66db9202b6841dc763d1726394"
+	dbname   = "d1ec1tpipgs10c"
 )
 
 type UserRepo struct {
@@ -22,19 +21,76 @@ type UserRepo struct {
 	Db   *sql.DB
 }
 
-func NewUserRepo() *UserRepo {
+func NewUserRepo() (*UserRepo, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s sslmode=disable",
 		host, port, user, password)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	err = db.Ping()
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	return &UserRepo{data: map[string]*User{}, Db: db}
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			user_id uuid NOT NULL,
+			username varchar(60) NOT NULL,
+			password varchar(60) NOT NULL,
+			created_on date NOT NULL DEFAULT NOW(),
+			CONSTRAINT PK_users PRIMARY KEY ( user_id )
+		)
+	`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS posts (
+			post_id    uuid NOT NULL,
+			score      int NOT NULL,
+			text       text NOT NULL,
+			title      varchar(255) NOT NULL,
+			category   varchar(255) NOT NULL,
+			created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			user_id    uuid NOT NULL,
+			CONSTRAINT PK_posts PRIMARY KEY ( post_id ),
+			CONSTRAINT FK_23 FOREIGN KEY ( user_id ) REFERENCES users ( user_id )
+		 )
+	`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS comments (
+			comment_id uuid NOT NULL,
+			post_id    uuid NOT NULL,
+			user_id    uuid NOT NULL,
+			text       text NOT NULL,
+			created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			CONSTRAINT PK_comments PRIMARY KEY ( comment_id ),
+			CONSTRAINT FK_39 FOREIGN KEY ( post_id ) REFERENCES posts ( post_id ),
+			CONSTRAINT FK_55 FOREIGN KEY ( user_id ) REFERENCES users ( user_id )
+		)
+	`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS votes (
+			post_id uuid NOT NULL,
+			user_id uuid NOT NULL,
+			CONSTRAINT FK_60 FOREIGN KEY ( post_id ) REFERENCES posts ( post_id ),
+			CONSTRAINT FK_65 FOREIGN KEY ( user_id ) REFERENCES users ( user_id )
+		)
+	`)
+	if err != nil {
+		return nil, err
+	}
+	return &UserRepo{data: map[string]*User{}, Db: db}, nil
 }
 
 func (repo *UserRepo) Register(u *User) error {
